fafc9b86998e8288842e4490845bc3467a08c501
Kevin Charnock <kevinshuldiner@gmail.com>
2024-05-18 18:36:50 -0300

    Adicionando comentarios no globalPipes

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,22 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
+
+      /* com transform, indicamos que queremos que o pipe transforme o JSON recebido na requisição
+       para um objeto da classe que vamos usar como tipo do parâmetro no método do controller
+       decorado com o @Body. */
       transform: true,
+
+      /* whitelist: true: Essa configuração indica que as chaves do JSON devem
+       ser iguais ao do objeto no qual o JSON será transformado,
+        ignorando chaves que não são pareáveis a atributos do objeto. */
       whitelist: true,
 
-      //Pipe de erro caso tenha info a mais no DTO
+      /*forbidNonWhitelisted: true: Através dessa configuração, indicamos que qualquer chave que vier que
+       não tiver par no objeto final deverá causar um erro, 
+       o que sinaliza que o cliente da nossa API está tentando enviar dados que não aceitamos. */
       forbidNonWhitelisted: true,
+      
     })
   );
   await app.listen(3000);
